0e76887e3f0a5a9b1ef674be725a2c2e3b7352a3
TerlajakPopular <putradany77@gmail.com>
2021-04-29 04:50:49 +0800

    Create Readme.m

---
+++ Wireguard Server/SSH SG 01/Readme.m
@@ -0,0 +1,11 @@
+[Interface]
+PrivateKey = MHrKXJjFfwQFgTIiTVAIoK3kHtfNuYLrSnHYhTZ+9no=
+Address = 10.9.4.39/24
+DNS = 8.8.8.8
+MTU = 1420
+[Peer]
+PublicKey = A4FOHzfcN9o8lUuHPfmNWgCDTB2HJjXQBVwHCfGMETM=
+AllowedIPs = 0.0.0.0/0, ::/0 
+Endpoint = wg-sg01.hostip.us:2640
+PersistentKeepalive = 25
+
